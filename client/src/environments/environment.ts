a8c510581b622ba69ebdccd1abbfe23d39c09e19
kelp-mohitk <mohit.k@kelpglobal.com>
2025-12-30 16:47:26 +0530

    -Added the automatic migration code -Added the nginx configuration for proxy.

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,7 +1,7 @@
 import { Socket } from "socket.io-client";
 
 export const environment = {
-  apiUrl: 'http://localhost:3000/api',
+  apiUrl: '/api',
   passwordEncryptionKey: 'my-super-secret-encryption-key-32ch',
-  socketUrl: 'http://localhost:3000',
+  socketUrl: '',
 };
--- server/src/constants/urls.ts
+++ server/src/constants/urls.ts
@@ -1,1 +1,1 @@
-export const FRONTEND_URL = "http://localhost:4200";
+export const FRONTEND_URL = "http://localhost";
